fcdc8778fdd0207fe8e1bbbc88529eea527b8ba7
shareeqshabeer <75534256+shareeqshabeer@users.noreply.github.com>
2022-01-08 17:05:52 +0530

    Add files via upload

---
+++ visualisation.py
@@ -0,0 +1,28 @@
+#import libraries
+import matplotlib.pyplot as plt 
+import pandas as pd
+import streamlit as st
+import numpy as np
+import matplotlib
+#matplotlib.use('Agg')
+import seaborn as sns 
+#Remove Warnings
+st.balloons()
+st.set_option('deprecation.showPyplotGlobalUse', False)
+st.title("Tips_Dataset")
+
+#import dataset
+df = pd.read_csv('tips.csv')
+#First thirty rows
+tips = df.head(20)
+#Display the table
+st.table(tips)
+st.header("Visualisation Using Seaborn")
+#bar plot
+st.subheader("Bar Plot")
+tips.plot(kind='bar')
+st.pyplot()
+#Displot
+st.subheader("Displot")
+sns.displot(tips['total_bill'])
+st.pyplot()
